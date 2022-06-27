d6864b858fd4e8ea404eb3e76797ead76b6c1ebf
phat.intern <coolcore.xix@gmail.com>
2022-06-27 21:47:45 +0700

    feat: gotta be happy with an http server

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ import express from "express";
 import multer from "multer";
 import { cropFromBuffer } from "./convert-screenshot/cropFromBuffer";
 import { ocrFromBlob } from "./convert-screenshot/ocrFromBuffer";
+import fs from 'fs';
+import http from 'http';
+import path from "path";
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 const app = express();
 app.use(cors());
 console.log("in the watch");
-
 app
   .use(upload.single("file"))
   .post("/api/convert-binance-screenshot", async (req, res) => {
@@ -20,6 +22,8 @@ app
     return res.send(textExtracted);
   });
 
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
-);
+const httpServer = http.createServer(app);
+
+httpServer.listen(process.env.PORT, () => {
+  console.log(`HTTPS Server running on port ${process.env.PORT}`);
+});
